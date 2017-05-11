9313a4a176fb4f25112bdf91bcc48d541d57ee9d
fangzheng <chenfangzheng@outlook.com>
2017-05-11 15:43:23 +0800

    encode url parameters

--- routes/qrcode.js
+++ routes/qrcode.js
@@ -5,10 +5,10 @@ var qr_image = require('qr-image');
 
 /* GET users listing. */
 router.get('/:first/:second/:third', function (req, res, next) {
-    var temp_qrcode = qr_image.image('http://www.baidu.com?'
-        + "first=" + req.params.first
-        + "&second=" + req.params.second
-        + "&third=" + req.params.third);
+    var temp_qrcode = qr_image.image('http://180.97.203.111:6001/mobile.html?'
+        + "first=" + encodeURIComponent(req.params.first)
+        + "&second=" + encodeURIComponent(req.params.second)
+        + "&third=" + encodeURIComponent(req.params.third));
     res.type('png');
     temp_qrcode.pipe(res);
 });
